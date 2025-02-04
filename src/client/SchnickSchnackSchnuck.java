03305cbe19f84936095b3cb5396a6df7245df38f
Noah Kemmesies <noahkemmesies@gmail.com>
2025-02-04 19:48:10 +0100

    First Version of SchnickSchnackSchnuck by Luiza

--- src/client/SchnickSchnackSchnuck.java
+++ src/client/SchnickSchnackSchnuck.java
@@ -1,6 +1,30 @@
 package client;
 
 public class SchnickSchnackSchnuck {
+    Connection connection;
 
-    //Main method and Connection object needed
+    public SchnickSchnackSchnuck() {
+        this.connection = new Connection(this);
+        mainMenu();
+    }
+
+    private void mainMenu() {
+        if (connection.connectToLocal()) {
+            connection.start();
+        }
+        System.out.println("Geben Sie \"Schere\", \"Stein\" oder \"Papier\" ein:");
+    }
+
+    public void receive(String message) {
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        new SchnickSchnackSchnuck();
+    }
+    /*
+    public static void main(String[] args) {
+        Connection connection = new Connection();
+        System.out.println("Geben Sie \"Schere\", \"Stein\" oder \"Papier\" ein:");
+    }*/
 }
